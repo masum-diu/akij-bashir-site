70b97054be349e8e2f1f8b9c795c1dc726d1b9bb
“masum182” <masum.mollah@webable.digital>
2023-01-18 15:37:50 +0600

    added bootsrap and route v6

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from "react";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import RoutePage from "./page/RoutePage";
 
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <div className="bg-danger">
+      <Header />
+      <div className="top" style={{ height: "100vh", paddingTop: "2rem" }}>
+        <RoutePage />
+      </div>
+      <Footer />
+    </div>
+  );
+};
 
-export default App
+export default App;
